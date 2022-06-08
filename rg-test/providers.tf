cc673272de6cee3218ec3022ba79dc36b8972e4a
Patrick Riedijk <patrick.riedijk@avanade.com>
2022-06-08 16:23:09 +0200

    added rg workflow'

--- rg-test/providers.tf
+++ rg-test/providers.tf
@@ -1,4 +1,9 @@
 terraform {
+  backend "azurerm" {
+    resource_group_name  = "tfstate-FAKKEL-test"
+    storage_account_name = "tfstatefakkeltest"
+    container_name       = "tfstatefakkeltest"
+  }
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
