45f816badedc63effaad0bea93773cc54c8be512
Patrick Riedijk <patrick.riedijk@avanade.com>
2022-06-20 11:29:49 +0200

    test

--- rg-test/locals.tf
+++ rg-test/locals.tf
@@ -1,4 +1,3 @@
 locals {
     rgname = "meepmeep"
-    kv_suffix = var.location_code == "weu" ? "56d5" : "6c49"
 }
--- rg-test/rg.tf
+++ rg-test/rg.tf
@@ -1,3 +1,7 @@
+locals {
+    kv_suffix = var.location_code == "weu" ? "56d5" : "6c49"
+}
+
 resource "random_pet" "rg-name" {
   prefix    = var.resource_group_name_prefix
 }
