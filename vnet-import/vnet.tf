ebcc7a03cd554bea19028fde3ab82ee1e461574c
Patrick Riedijk <patrick.riedijk@avanade.com>
2022-08-18 16:31:53 +0200

    removed bloat

--- vnet-import/vnet.tf
+++ vnet-import/vnet.tf
@@ -1,18 +1,3 @@
-/*
-locals {
-  subnets_flatlist = flatten([for key, val in var.network : [
-    for subnet in val.subnets : {
-      vnet_name      = key
-      subnet_name    = subnet.subnet_name
-      subnet_address = subnet.subnet_address
-    }
-    ]
-  ])
-
-  subnets = { for subnet in local.subnets_flatlist : subnet.subnet_name => subnet }
-}
-*/
-
 resource "azurerm_resource_group" "vnet-rg" {
   name     = "tf-import-test"
   location = var.resource_group_location
@@ -24,14 +9,7 @@ resource "azurerm_virtual_network" "import-vnet" {
   location            = azurerm_resource_group.vnet-rg.location
   address_space       = ["10.20.0.0/16"]
 }
-/*
-resource "azurerm_virtual_network" "import-vnet" {
-  name                = "import-vnet"
-  location            = azurerm_resource_group.vnet-rg.location
-  resource_group_name = azurerm_resource_group.vnet-rg.name
-  address_space       = ["10.20.0.0/16"]
-}
-*/
+
 resource "azurerm_subnet" "subnets" {
   for_each             = var.network
   name                 = each.value.subnet_name
@@ -39,18 +17,3 @@ resource "azurerm_subnet" "subnets" {
   virtual_network_name = azurerm_virtual_network.import-vnet.name
   address_prefixes     = [each.value.subnet_address]
 }
-
-/*
-resource "azurerm_subnet" "firewall" {
-  name                 = "AzureFirewallSubnet"
-  resource_group_name  = azurerm_resource_group.vnet-rg.name
-  virtual_network_name = azurerm_virtual_network.import-vnet.name
-  address_prefixes     = ["10.20.0.0/27"]
-}
-resource "azurerm_subnet" "gateway" {
-  name                 = "GatewaySubnet"
-  resource_group_name  = azurerm_resource_group.vnet-rg.name
-  virtual_network_name = azurerm_virtual_network.import-vnet.name
-  address_prefixes     = ["10.20.0.32/27"]
-}
-*/
