a21469d43b8722ea09cb879ceab37adffdb75320
Patrick Riedijk <patrick.riedijk@avanade.com>
2022-08-18 16:20:32 +0200

    for each test

--- vnet-import/vnet.tf
+++ vnet-import/vnet.tf
@@ -17,7 +17,7 @@ resource "azurerm_resource_group" "vnet-rg" {
 }
 
 resource "azurerm_virtual_network" "import-vnet" {
-  name                = import-vnet
+  name                = "import-vnet"
   resource_group_name = azurerm_resource_group.vnet-rg.name
   location            = azurerm_resource_group.vnet-rg.location
   address_space       = ["10.20.0.0/16"]
