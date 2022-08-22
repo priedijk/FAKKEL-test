db7e2a76a0f8f371c1d686920b11d78ca9b4b2db
Patrick Riedijk <patrick.riedijk@avanade.com>
2022-08-22 14:26:29 +0200

    added bastion subnet

--- vnet-import/vnet.tf
+++ vnet-import/vnet.tf
@@ -28,7 +28,7 @@ resource "azurerm_subnet" "subnets" {
 }
 
 resource "azurerm_subnet" "subnet-test" {
-  name                 = "subnetname1"
+  name                 = "AzureBastionSubnet"
   resource_group_name  = azurerm_resource_group.vnet-rg.name
   virtual_network_name = azurerm_virtual_network.import-vnet.name
   address_prefixes     = [var.network_weu_ae.firewall.bastion]
