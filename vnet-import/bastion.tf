93741d938ee7b07f2ef6073d3ecb30769f2a5c6e
Patrick Riedijk <patrick.riedijk@avanade.com>
2022-08-22 14:29:02 +0200

    added Bastion

---
+++ vnet-import/bastion.tf
@@ -0,0 +1,19 @@
+resource "azurerm_public_ip" "bastion_pip" {
+  name                = "bastion-pip"
+  location            = azurerm_resource_group.vnet-rg.location
+  resource_group_name = azurerm_resource_group.vnet-rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_bastion_host" "bastion" {
+  name                = "bast-shared-hub-${var.location}-001"
+  location            = azurerm_resource_group.vnet-rg.location
+  resource_group_name = azurerm_resource_group.vnet-rg.name
+
+  ip_configuration {
+    name                 = "IpConf"
+    subnet_id            = azurerm_subnet.subnet-test.id
+    public_ip_address_id = azurerm_public_ip.bastion_pip.id
+  }
+}
