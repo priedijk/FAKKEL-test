076075f4f6077e851fdac9b648cadeabb474230a
Patrick Riedijk <patrick.riedijk@avanade.com>
2022-08-22 15:47:22 +0200

    variable fix

--- vnet-import/local_gateway.tf
+++ vnet-import/local_gateway.tf
@@ -6,8 +6,8 @@ locals {
 
 resource "azurerm_local_network_gateway" "gateway_local" {
   name                = "lgateway-${var.location}-001"
-  location            = azurerm_resource_group.rg_hub.location
-  resource_group_name = azurerm_resource_group.rg_hub.name
+  location            = azurerm_resource_group.vnet-rg.location
+  resource_group_name = azurerm_resource_group.vnet-rg.name
   gateway_address     = local.local_gateway_ip_address
   address_space       = local.local_address_space
 }
