2c2196baa99a27fad830c589361f067ece02a97e
Patrick Riedijk <patrick.riedijk@avanade.com>
2022-08-24 11:03:49 +0200

    added for each delgation

--- vnet-import/vnet.tf
+++ vnet-import/vnet.tf
@@ -30,8 +30,8 @@ resource "azurerm_subnet" "subnets" {
   address_prefixes     = [each.value.subnet_address]
 
   dynamic "delegation" {
-    for_each = { for k, v in local.subnets : k => v if lookup(v, "service_delegation", "") != null }
-
+    for_each = local.subnets.service_delegation == null ? [] : [1]
+    #for_each = { for k, v in local.subnets : k => v if lookup(v, "service_delegation", "") != null }
     content {
       name = "delegation"
 
