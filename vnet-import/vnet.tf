67fea1907dd4988c3cfbd81c58b3949694f7765a
Patrick Riedijk <patrick.riedijk@avanade.com>
2022-08-24 10:19:42 +0200

    multiple delegations

--- vnet-import/vnet.tf
+++ vnet-import/vnet.tf
@@ -32,7 +32,7 @@ resource "azurerm_subnet" "subnets" {
 
 
 resource "azurerm_subnet" "subnet-test" {
-  name                 = "AzureBastionSubnet"
+  name                 = "delegation-net"
   resource_group_name  = azurerm_resource_group.vnet-rg.name
   virtual_network_name = azurerm_virtual_network.import-vnet.name
   address_prefixes     = [var.network_weu_ae.AzureFirewallSubnet.bastion]
