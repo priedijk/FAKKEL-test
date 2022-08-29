daab25e0072b38a53e525e5b7b1c8a0837d3ca5c
Patrick Riedijk <patrick.riedijk@avanade.com>
2022-08-29 14:21:56 +0200

    tfvars test

--- rg-test/rg.tf
+++ rg-test/rg.tf
@@ -3,11 +3,12 @@ locals {
   choose_name_1 = "name1-chosen"
   choose_name_2 = "name2-chosen"
 }
+
 resource "random_pet" "rg-name" {
   prefix = var.resource_group_name_prefix
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-${local.chooser}"
+  name     = "rg-${var.rg_name}"
   location = var.resource_group_location
 }
