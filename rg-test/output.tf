aaf23ee02287075a9558d7444987596d0b2214e1
Patrick Riedijk <patrick.riedijk@avanade.com>
2022-08-29 15:48:18 +0200

    replace test

--- rg-test/output.tf
+++ rg-test/output.tf
@@ -6,5 +6,5 @@ output "range" {
   value = var.address_space.vnet
 }
 output "range-replaced" {
-  value = replace(var.address_space.vnet, "0.0/24", "0.64/26")
+  value = replace(var.address_space.vnet, var.address_space.replace, var.address_space.test)
 }
--- rg-test/test.tfvars
+++ rg-test/test.tfvars
@@ -1,4 +1,6 @@
 address_space = {
     "vnet"      ="10.100.0.0/24"
+    "replace"   ="0.0/24"
+    "test"      ="0.64/26"
     "rg_name"   ="pluto"
 }
