8bd9bfacb5ae1169e563ef4ee448cb673d229904
Patrick Riedijk <patrick.riedijk@avanade.com>
2022-08-29 16:28:41 +0200

    added vnet test'

--- rg-test/rg.tf
+++ rg-test/rg.tf
@@ -2,6 +2,8 @@ locals {
   chooser = var.location_code == "frc" ? local.choose_name_1 : local.choose_name_2
   choose_name_1 = "name1-chosen"
   choose_name_2 = "name2-chosen"
+  dns1 = "10.20.0.1"
+  dns2 = "10.20.0.4"
 }
 
 resource "random_pet" "rg-name" {
@@ -11,4 +13,12 @@ resource "random_pet" "rg-name" {
 resource "azurerm_resource_group" "rg" {
   name     = "rg-${var.address_space.rg_name}"
   location = var.resource_group_location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "rg-vnet-te"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = [replace(var.address_space.vnet, "100.0.0/24", "140.0.0/24")]
+  dns_servers         = [local.dns1, local.dns2]
 }
