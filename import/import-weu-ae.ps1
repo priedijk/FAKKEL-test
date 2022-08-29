faa9c9a7ef4a3f7c54c24d0a3ad28060cb687343
Patrick Riedijk <patrick.riedijk@avanade.com>
2022-08-29 10:23:49 +0200

    variable script

--- import/import-weu-ae.ps1
+++ import/import-weu-ae.ps1
@@ -1,7 +1,7 @@
-echo $ARM_SUBSCRIPTION_ID
-
+echo $(ARM_SUBSCRIPTION_ID)
+echo "$(ARM_SUBSCRIPTION_ID)"
 
 #terraform import azurerm_resource_group.vnet-rg "/subscriptions/e2c1b56d-a413-43fc-b1e2-f73e153c05ad/resourceGroups/tf-import-test"
 #terraform import azurerm_virtual_network.import-vnet "/subscriptions/e2c1b56d-a413-43fc-b1e2-f73e153c05ad/resourceGroups/tf-import-test/providers/Microsoft.Network/virtualNetworks/import-vnet"
-terraform import azurerm_subnet.subnets['\"firewall\"'] "/subscriptions/$ARM_SUBSCRIPTION_ID/resourceGroups/tf-import-test/providers/Microsoft.Network/virtualNetworks/import-vnet/subnets/AzureFirewallSubnet"
-terraform import azurerm_subnet.subnets['\"gateway\"'] "/subscriptions/$ARM_SUBSCRIPTION_ID/resourceGroups/tf-import-test/providers/Microsoft.Network/virtualNetworks/import-vnet/subnets/GatewaySubnet"
+terraform import azurerm_subnet.subnets['\"firewall\"'] "/subscriptions/$(ARM_SUBSCRIPTION_ID)/resourceGroups/tf-import-test/providers/Microsoft.Network/virtualNetworks/import-vnet/subnets/AzureFirewallSubnet"
+terraform import azurerm_subnet.subnets['\"gateway\"'] "/subscriptions/$(ARM_SUBSCRIPTION_ID)/resourceGroups/tf-import-test/providers/Microsoft.Network/virtualNetworks/import-vnet/subnets/GatewaySubnet"
