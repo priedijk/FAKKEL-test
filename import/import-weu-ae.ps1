0a0573d40af71d5e5495191e9d28259740ffc1bf
Patrick Riedijk <patrick.riedijk@avanade.com>
2022-08-29 11:16:10 +0200

    variable script

--- import/import-weu-ae.ps1
+++ import/import-weu-ae.ps1
@@ -5,7 +5,6 @@ param(
     [String] [Parameter (Mandatory = $true)]  $tenant
 )
 
-echo ${ARM_SUBSCRIPTION_ID}
 echo ${subscription}
 echo $location
 echo $tenant
@@ -13,4 +12,4 @@ echo $tenant
 #terraform import azurerm_resource_group.vnet-rg "/subscriptions/e2c1b56d-a413-43fc-b1e2-f73e153c05ad/resourceGroups/tf-import-test"
 #terraform import azurerm_virtual_network.import-vnet "/subscriptions/e2c1b56d-a413-43fc-b1e2-f73e153c05ad/resourceGroups/tf-import-test/providers/Microsoft.Network/virtualNetworks/import-vnet"
 terraform import azurerm_subnet.subnets['\"firewall\"'] "/subscriptions/${subscription}/resourceGroups/tf-import-test/providers/Microsoft.Network/virtualNetworks/import-vnet/subnets/AzureFirewallSubnet"
-terraform import azurerm_subnet.subnets['\"gateway\"'] "/subscriptions/${subscription}/resourceGroups/tf-import-test/providers/Microsoft.Network/virtualNetworks/import-vnet/subnets/GatewaySubnet"
+terraform import azurerm_subnet.subnets['\"gateway\"'] "/subscriptions/${subscription}/resourceGroups/tf-import-${location}/providers/Microsoft.Network/virtualNetworks/import-vnet/subnets/GatewaySubnet"
