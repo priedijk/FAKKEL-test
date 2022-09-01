b64b37de2594ecfc432d56dcc03c242a24123c2a
Patrick Riedijk <patrick.riedijk@avanade.com>
2022-09-01 16:01:58 +0200

    updated import script

--- import/import-weu-ae.ps1
+++ import/import-weu-ae.ps1
@@ -16,11 +16,11 @@ terraform import azurerm_subnet.subnets['\"GatewaySubnet\"'] "/subscriptions/${s
 terraform import azurerm_subnet.subnets['\"troep\"'] "/subscriptions/${subscription}/resourceGroups/tf-import-test/providers/Microsoft.Network/virtualNetworks/import-vnet/subnets/troep"
 
 
-terraform import azurerm_network_security_group.nsg['\apim\"'] "/subscriptions/${subscription}/resourceGroups/tf-import-test/providers/Microsoft.Network/networkSecurityGroups/nsg_ap_weu"
-terraform import azurerm_network_security_group.nsg['\apim_ingress\"'] "/subscriptions/${subscription}/resourceGroups/tf-import-test/providers/Microsoft.Network/networkSecurityGroups/nsg_api_weu"
-terraform import azurerm_network_security_group.nsg['\weballow\"'] "/subscriptions/${subscription}/resourceGroups/tf-import-test/providers/Microsoft.Network/networkSecurityGroups/nsg_web_weu"
+terraform import azurerm_network_security_group.nsg['\"apim\"'] "/subscriptions/${subscription}/resourceGroups/tf-import-test/providers/Microsoft.Network/networkSecurityGroups/nsg_ap_weu"
+terraform import azurerm_network_security_group.nsg['\"apim_ingress\"'] "/subscriptions/${subscription}/resourceGroups/tf-import-test/providers/Microsoft.Network/networkSecurityGroups/nsg_api_weu"
+terraform import azurerm_network_security_group.nsg['\"weballow\"'] "/subscriptions/${subscription}/resourceGroups/tf-import-test/providers/Microsoft.Network/networkSecurityGroups/nsg_web_weu"
 
 azurerm_network_security_rule.nsg_rules_bastion2  "/subscriptions/${subscription}/resourceGroups/tf-import-test/providers/Microsoft.Network/networkSecurityGroups/nsg_web_weu/securityRules/portranges"
 
-terraform import azurerm_network_security_group.nsg['\weballow\"'] "/subscriptions/${subscription}/resourceGroups/tf-import-test/providers/Microsoft.Network/networkSecurityGroups/nsg_web_weu"
-terraform import azurerm_subnet_network_security_group_association.assoc['\apim\"'] "/subscriptions/${subscription}/resourceGroups/tf-import-test/providers/Microsoft.Network/virtualNetworks/import-vnet/subnets/troep"
+terraform import azurerm_network_security_group.nsg['\"weballow\"'] "/subscriptions/${subscription}/resourceGroups/tf-import-test/providers/Microsoft.Network/networkSecurityGroups/nsg_web_weu"
+terraform import azurerm_subnet_network_security_group_association.assoc['\"apim\"'] "/subscriptions/${subscription}/resourceGroups/tf-import-test/providers/Microsoft.Network/virtualNetworks/import-vnet/subnets/troep"
