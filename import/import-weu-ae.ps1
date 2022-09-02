d1a938abc57bb2e5533b830fb15341b9425b825f
Patrick Riedijk <patrick.riedijk@avanade.com>
2022-09-02 14:50:24 +0200

    import test

--- import/import-weu-ae.ps1
+++ import/import-weu-ae.ps1
@@ -6,14 +6,16 @@ param(
     [String] [Parameter (Mandatory = $true)]  $terraformdir
 )
 
-$import="terraform -chdir=${terraformdir} import"
+#$import="terraform -chdir=${terraformdir} import"
+$import="terraform -chdir=${terraformdir}"
 
 
 echo ${subscription}
 echo $location
 echo $tenant
 
-& $import azurerm_resource_group.vnet-rg "/subscriptions/e2c1b56d-a413-43fc-b1e2-f73e153c05ad/resourceGroups/tf-import-test"
+#& $import azurerm_resource_group.vnet-rg "/subscriptions/e2c1b56d-a413-43fc-b1e2-f73e153c05ad/resourceGroups/tf-import-test"
+& $import import azurerm_resource_group.vnet-rg "/subscriptions/e2c1b56d-a413-43fc-b1e2-f73e153c05ad/resourceGroups/tf-import-test"
 
 & terraform import azurerm_resource_group.vnet-rg "/subscriptions/e2c1b56d-a413-43fc-b1e2-f73e153c05ad/resourceGroups/tf-import-test"
 
