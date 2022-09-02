06e3e7504f688f62ba4833bf05e8631e2c79737f
Patrick Riedijk <patrick.riedijk@avanade.com>
2022-09-02 15:27:19 +0200

    quotes

--- import/import-weu-ae.ps1
+++ import/import-weu-ae.ps1
@@ -6,11 +6,15 @@ param(
     [String] [Parameter (Mandatory = $true)]  $terraformdir
 )
 
-$import="terraform -chdir=${terraformdir} import"
-echo $terraformdir
+$import="terraform -chdir="${terraformdir}" import"
 
-terraform -chdir=${terraformdir} import azurerm_resource_group.vnet-rg "/subscriptions/e2c1b56d-a413-43fc-b1e2-f73e153c05ad/resourceGroups/tf-import-test"
+#$terraformdir="..\kv-test"
+
+terraform -chdir="${terraformdir}" import azurerm_resource_group.vnet-rg "/subscriptions/e2c1b56d-a413-43fc-b1e2-f73e153c05ad/resourceGroups/tf-import-test"
+& $import azurerm_resource_group.vnet-rg "/subscriptions/e2c1b56d-a413-43fc-b1e2-f73e153c05ad/resourceGroups/tf-import-test"
+
 exit
+
 terraform import azurerm_resource_group.vnet-rg "/subscriptions/e2c1b56d-a413-43fc-b1e2-f73e153c05ad/resourceGroups/tf-import-test"
 
 terraform import azurerm_virtual_network.import-vnet "/subscriptions/e2c1b56d-a413-43fc-b1e2-f73e153c05ad/resourceGroups/tf-import-test/providers/Microsoft.Network/virtualNetworks/import-vnet"
