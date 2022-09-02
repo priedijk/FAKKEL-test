0b47151a0ac3885608d1ee953752671e48de8ea3
Patrick Riedijk <patrick.riedijk@avanade.com>
2022-09-02 12:07:19 +0200

    count test

--- rg-test/rg.tf
+++ rg-test/rg.tf
@@ -1,5 +1,5 @@
 locals {
-  chooser = var.location_code == "frc" ? local.choose_name_1 : local.choose_name_2
+  chooser       = var.location_code == "frc" ? local.choose_name_1 : local.choose_name_2
   choose_name_1 = "name1-chosen"
   choose_name_2 = "name2-chosen"
 }
@@ -9,6 +9,7 @@ resource "random_pet" "rg-name" {
 }
 
 resource "azurerm_resource_group" "rg" {
+  count    = var.location_code == "frc" ? 0 : 1
   name     = "rg-${var.address_space.rg_name}"
   location = var.resource_group_location
 }
