248c0bdaaeefcfdc4d04462ae655d5ef3e0a6d51
Patrick Riedijk <patrick.riedijk@avanade.com>
2022-09-12 10:33:21 +0200

    remove output shared key

--- rg-test/output.tf
+++ rg-test/output.tf
@@ -8,7 +8,3 @@ output "range" {
 output "range-replaced" {
   value = replace(var.address_space.vnet, var.address_space.replace, var.address_space.test)
 }
-
-output "shared_key" {
-  value = var.shared_key
-}
