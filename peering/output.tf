bfd13223a4abcd9400d94b24d604384686b32adb
Patrick Riedijk <patrick.riedijk@avanade.com>
2022-09-13 15:31:43 +0200

    fix

--- peering/output.tf
+++ peering/output.tf
@@ -7,17 +7,16 @@ resource "azurerm_resource_group" "output" {
   name     = "output-tester"
   location = "westeurope"
   tags = {
-    key1 = data.azurerm_virtual_network.vnet1.vnet_peerings[0]
-    key2 = data.azurerm_virtual_network.vnet1.vnet_peerings.id
+    key1 = data.azurerm_virtual_network.vnet1.vnet_peerings
   }
 }
 
 output "peering1" {
   value = data.azurerm_virtual_network.vnet1.vnet_peerings
 }
-output "peering1-1" {
-  value = data.azurerm_virtual_network.vnet1.vnet_peerings.id
-}
+# output "peering1-1" {
+#   value = data.azurerm_virtual_network.vnet1.vnet_peerings.id
+# }
 
 # output "peering1-2" {
 #   value = data.azurerm_virtual_network.vnet1.vnet_peerings[each.key]
