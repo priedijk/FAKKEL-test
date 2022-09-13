16d288c3cc920a0c0af591edf82935d8ccd22114
Patrick Riedijk <patrick.riedijk@avanade.com>
2022-09-13 14:52:56 +0200

    name fix

--- peering/peering.tf
+++ peering/peering.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network_peering" "peer-1" {
   name                      = "peer1to2"
-  resource_group_name       = azurerm_resource_group.rg.location
+  resource_group_name       = azurerm_resource_group.rg.name
   virtual_network_name      = azurerm_virtual_network.vnet1.name
   remote_virtual_network_id = azurerm_virtual_network.vnet2.id
 }
