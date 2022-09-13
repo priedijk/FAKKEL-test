78b0bb49ce190a65f1e4b65c0b25d4df9ad06065
Patrick Riedijk <patrick.riedijk@avanade.com>
2022-09-13 14:57:17 +0200

    test

--- peering/output.tf
+++ peering/output.tf
@@ -4,9 +4,15 @@ data "azurerm_virtual_network" "vnet1" {
 }
 
 output "peering1" {
-  value = data.azurerm_virtual_network.vnet1.vnet_peerings
+  value = data.azurerm_virtual_network.vnet1.vnet_peerings.id
+}
+output "peering1-1" {
+  value = data.azurerm_virtual_network.vnet1.vnet_peerings[each.key]
 }
 
 output "peering2" {
-  value = data.azurerm_virtual_network.vnet1.vnet_peerings_addresses
+  value = data.azurerm_virtual_network.vnet1.vnet_peerings_addresses.id
+}
+output "peering2" {
+  value = data.azurerm_virtual_network.vnet1.vnet_peerings_addresses[each.key]
 }
