63a1a1661834fc2f4a5cb465e98d7e55fde85d8a
Patrick Riedijk <patrick.riedijk@avanade.com>
2022-09-20 12:09:16 +0200

    added set-env script

---
+++ scripts/set-workflow-env-variables.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# source env file and set values as environment variables
+while read -r line
+do
+    echo "$line" >> $GITHUB_ENV
+    export $line
+done < ./conf/$1.env
+
+# determine TF_VAR_location
+case $2 in
+    weu)
+    echo "TF_VAR_location=westeurope" >> $GITHUB_ENV
+    ;;
+    frc)
+    echo "TF_VAR_location=francecentral" >> $GITHUB_ENV
+    ;;
+esac
+
+
+echo "TF_CLI_ARGS_plan=$(echo $TF_CLI_ARGS_plan | envsubst)" >> $GITHUB_ENV
+echo "TF_CLI_ARGS_apply=$(echo $TF_CLI_ARGS_apply | envsubst)" >> $GITHUB_ENV
+echo "TF_CLI_ARGS_destroy=$(echo $TF_CLI_ARGS_destroy | envsubst)" >> $GITHUB_ENV
