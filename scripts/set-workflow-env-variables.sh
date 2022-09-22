67c78e36392c7304e396646e27ba3c468031a981
Patrick Riedijk <patrick.riedijk@avanade.com>
2022-09-22 13:47:19 +0200

    test

--- scripts/set-workflow-env-variables.sh
+++ scripts/set-workflow-env-variables.sh
@@ -18,8 +18,9 @@ case $2 in
 esac
 
 # echo "TF_VAR_shared_key=$VPN_SECRET" >> $GITHUB_ENV
-# echo "TF_VAR_testput1=$VPN_SECRET" >> $GITHUB_ENV
-# echo "TF_VAR_testput2=${VPN_SECRET}" >> $GITHUB_ENV
+ echo "TF_VAR_testput1=$VPN_SECRET" >> $GITHUB_ENV
+ echo "TF_VAR_testput2=${VPN_SECRET}" >> $GITHUB_ENV
+ echo "TF_VAR_testput3="${VPN_SECRET}"" >> $GITHUB_ENV
 
 echo "TF_CLI_ARGS_plan=$(echo $TF_CLI_ARGS_plan | envsubst)" >> $GITHUB_ENV
 echo "TF_CLI_ARGS_apply=$(echo $TF_CLI_ARGS_apply | envsubst)" >> $GITHUB_ENV
