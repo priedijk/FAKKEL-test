997322950d1e833f866e387584057e6193815826
Patrick Riedijk <patrick.riedijk@avanade.com>
2022-09-22 15:59:40 +0200

    test

--- peering/vnet.tf
+++ peering/vnet.tf
@@ -1,48 +1,51 @@
 locals {
+  vnet_range  = var.location_code == "weu" ? local.vnet_range1 : local.vnet_range2
   vnet_range1 = "10.20.0.0/24"
   vnet_range2 = "10.30.0.0/24"
+
+}
+
+data "azurerm_virtual_network" "vnet_weu" {
+  count               = var.location_code == "frc" ? 1 : 0
+  name                = "rg-${var.location_code}-peering"
+  resource_group_name = "rg-${var.location_code}-peering"
+}
+data "azurerm_virtual_network" "vnet_frc" {
+  count               = var.location_code == "weu" ? 1 : 0
+  name                = "rg-${var.location_code}-peering"
+  resource_group_name = "rg-${var.location_code}-peering"
 }
 
 resource "azurerm_resource_group" "rg" {
-
   name     = "rg-${var.location_code}-peering"
   location = var.resource_group_location
 }
 
-resource "azurerm_virtual_network" "vnet1" {
-  name                = "rg-vnet1"
+resource "azurerm_virtual_network" "vnet_weu" {
+  name                = "rg-${var.location_code}-peering"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  address_space       = [local.vnet_range1]
+  address_space       = [local.vnet_range]
 }
 
-resource "azurerm_subnet" "tb" {
-  name                 = "tbsubnet"
+resource "azurerm_subnet" "subnet1" {
+  name                 = "subnet1"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet1.name
   address_prefixes     = [replace(local.vnet_range1, "0.0/24", "0.176/28")]
-  service_endpoints    = var.location_code == "weu" && var.tenant == "ae" ? ["Microsoft.Storage"] : null
 }
 
-resource "azurerm_subnet" "tb1" {
-  name                 = "tb1subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet1.name
-  address_prefixes     = [replace(local.vnet_range1, "0.0/24", "0.0/28")]
-  service_endpoints    = var.location_code == "weu" && var.tenant == "ae" ? ["Microsoft.Storage"] : null
-}
+# resource "azurerm_virtual_network" "vnet_frc" {
+#   name                = "rg-france"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = "francecentral"
+#   address_space       = [local.vnet_range2]
+# }
 
-resource "azurerm_virtual_network" "vnet2" {
-  name                = "rg-vnet2"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  address_space       = [local.vnet_range2]
-}
-
-resource "azurerm_subnet" "subnet2" {
-  name                 = "subnet2"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet2.name
-  address_prefixes     = [replace(local.vnet_range2, "0.0/24", "0.176/28")]
-  service_endpoints    = var.location_code == "weu" && var.tenant == "ae" ? ["Microsoft.Storage"] : null
-}
+# resource "azurerm_subnet" "subnet2" {
+#   name                 = "subnet2"
+#   resource_group_name  = azurerm_resource_group.rg.name
+#   virtual_network_name = azurerm_virtual_network.vnet2.name
+#   address_prefixes     = [replace(local.vnet_range2, "0.0/24", "0.176/28")]
+#   service_endpoints    = var.location_code == "weu" && var.tenant == "ae" ? ["Microsoft.Storage"] : null
+# }
