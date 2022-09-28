b706164906bf099bab03d88366e33ce50ccb8cf1
Patrick Riedijk <patrick.riedijk@avanade.com>
2022-09-28 17:23:19 +0200

    random id import

---
+++ import/random.ps1
@@ -0,0 +1,9 @@
+[CmdletBinding()]
+param(
+    [String] [Parameter (Mandatory = $true)]  $subscription,
+    [String] [Parameter (Mandatory = $true)]  $location,
+    [String] [Parameter (Mandatory = $true)]  $terraformdir
+)
+
+
+terraform -chdir="${terraformdir}" import random_id.random "VtU"
--- random_test/random_id.tf
+++ random_test/random_id.tf
@@ -2,15 +2,20 @@
 resource "random_id" "random" {
   byte_length = 3
 }
+resource "random_id" "import" {
+  byte_length = 2
+}
 
 resource "azurerm_resource_group" "secret" {
   name     = "random-id-test"
   location = "westeurope"
   tags = {
-    test1 = "key-${lower(random_id.random.b64_std)}" # "test1": "key-g8e=",
-    test2 = "key-${lower(random_id.random.b64_url)}" # "test2": "key-g8e",
-    test3 = "key-${lower(random_id.random.dec)}"     # "test3": "key-33729",
-    test4 = "key-${lower(random_id.random.hex)}"     # "test4": "key-83c1",
-    test5 = "key-${lower(random_id.random.id)}"      # "test5": "key-g8e"
+    test1   = "key-${lower(random_id.random.b64_std)}" # "test1": "key-mhfl"
+    test2   = "key-${lower(random_id.random.b64_url)}" # "test2": "key-mhfl"
+    test3   = "key-${lower(random_id.random.dec)}"     # "test3": "key-3282891"
+    test4   = "key-${lower(random_id.random.hex)}"     # "test4": "key-3217cb"
+    test5   = "key-${lower(random_id.random.id)}"      # "test5": "key-mhfl"
+    import  = "key-${lower(random_id.import.id)}"      # "test5": "key-mhfl"
+    import2 = "key-${lower(random_id.import.hex)}"     # "test5": "key-mhfl"
   }
 }
