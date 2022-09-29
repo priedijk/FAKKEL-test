1d04901286908aa62044f276bce4a780c7b4e34e
Patrick Riedijk <patrick.riedijk@avanade.com>
2022-09-29 09:40:50 +0200

    keyvault without purge

--- kv-test/providers.tf
+++ kv-test/providers.tf
@@ -18,8 +18,8 @@ terraform {
 
 provider "azurerm" {
   features {
-    key_vault {
-      purge_soft_delete_on_destroy = true
-    }
+    # key_vault {
+    #   purge_soft_delete_on_destroy = true
+    # }
   }
 }
