9604771cd3d386a0d19c42d97ac17bbedebefc3d
Patrick Riedijk <patrick.riedijk@avanade.com>
2022-09-29 10:41:25 +0200

    " in true

--- peering/peering.tf
+++ peering/peering.tf
@@ -1,11 +1,11 @@
 data "azurerm_virtual_network" "vnet" {
-  count               = var.new_deployment == "true" ? 0 : 1
+  count               = var.new_deployment == true ? 0 : 1
   name                = "rg-${local.inverter}-peering"
   resource_group_name = "rg-${local.inverter}-peering"
 }
 
 resource "azurerm_virtual_network_peering" "peering" {
-  count                     = var.new_deployment == "true" ? 0 : 1
+  count                     = var.new_deployment == true ? 0 : 1
   name                      = "perfrom-${var.location_code}-to-${local.inverter}"
   resource_group_name       = azurerm_resource_group.rg.name
   virtual_network_name      = azurerm_virtual_network.vnet.name
