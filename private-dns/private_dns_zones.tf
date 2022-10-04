ee2e54477bd3cb8313dff8e4dbb5dfed13fa21f0
Patrick Riedijk <patrick.riedijk@avanade.com>
2022-10-04 10:53:56 +0200

    test

--- private-dns/private_dns_zones.tf
+++ private-dns/private_dns_zones.tf
@@ -20,7 +20,7 @@ resource "azurerm_resource_group" "hub_private_dns" {
 
 resource "azurerm_private_dns_zone" "hub" {
   # count               = var.location_code == "frc" ? 0 : 1
-  for_each            = var.location_code == "frc" ? toset(var.dns_zones) : {}
+  for_each            = var.location_code == "frc" ? toset(var.dns_zones) : []
   name                = each.value
   resource_group_name = local.private_dns_rg_name
 }
