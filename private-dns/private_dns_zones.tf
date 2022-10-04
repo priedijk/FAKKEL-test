2ee819f57d55f402bc2a60a6182be9e232078ab5
Patrick Riedijk <patrick.riedijk@avanade.com>
2022-10-04 10:58:20 +0200

    vnet.id

--- private-dns/private_dns_zones.tf
+++ private-dns/private_dns_zones.tf
@@ -30,7 +30,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "hub" {
   name                  = "${each.value}-hub-vnet-${var.location_code}"
   resource_group_name   = local.private_dns_rg_name
   private_dns_zone_name = each.value
-  virtual_network_id    = local.vnet_data
+  virtual_network_id    = local.vnet_data.id
   lifecycle {
     ignore_changes = [
       name
