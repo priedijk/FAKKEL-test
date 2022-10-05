76af0b4c4731c7f21b37c5082819ffee32eff797
Patrick Riedijk <patrick.riedijk@avanade.com>
2022-10-05 15:13:09 +0200

    added backend

--- aks/provider.tf
+++ aks/provider.tf
@@ -1,16 +1,9 @@
-provider "azurerm" {
-  features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false
-    }
-  }
-}
-
-resource "random_id" "nr" {
-  byte_length = 4
-}
-
 terraform {
+  backend "azurerm" {
+    resource_group_name  = "tffakkeltest"
+    storage_account_name = "tffakkeltest"
+    container_name       = "tfstate"
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -23,3 +16,14 @@ terraform {
   }
 }
 
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+resource "random_id" "nr" {
+  byte_length = 4
+}
