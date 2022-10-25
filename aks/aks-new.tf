2fcc8f4ce9e98828f7207c65da795cbcce73fa1e
Patrick Riedijk <patrick.riedijk@avanade.com>
2022-10-25 11:26:29 +0200

    test new

---
+++ aks/aks-new.tf
@@ -0,0 +1,93 @@
+resource "azurerm_resource_group" "aks" {
+  name     = "aks-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "userpool" {
+  name                  = "user"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
+  vm_size               = "Standard_DS2_v2"
+  min_count             = 1
+  max_count             = 2
+  #availability_zones   = ["1", "2", "3"]
+  vnet_subnet_id      = azurerm_subnet.userpool.id
+  enable_auto_scaling = true
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                      = "aks-test1"
+  location                  = azurerm_resource_group.aks.location
+  resource_group_name       = azurerm_resource_group.aks.name
+  dns_prefix                = "aks-test1"
+  automatic_channel_upgrade = "node-image"
+  private_dns_zone_id       = "None"
+  kubernetes_version        = "1.23.12"
+
+  default_node_pool {
+    name                         = "default"
+    min_count                    = 1
+    max_count                    = 3
+    vm_size                      = "Standard_DS2_v2"
+    vnet_subnet_id               = azurerm_subnet.systempool.id
+    enable_auto_scaling          = true
+    only_critical_addons_enabled = true
+    #availability_zones          = ["1", "2", "3"]
+  }
+
+  oms_agent {
+    log_analytics_workspace_id = azurerm_log_analytics_workspace.hub.id
+  }
+
+  azure_active_directory_role_based_access_control {
+    managed = true
+    # admin_group_object_ids = ["7ce4b940-3b71-4a60-a935-edec80b406b4"]
+    azure_rbac_enabled = true
+  }
+
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.aks.id]
+  }
+
+  network_profile {
+    network_plugin     = "kubenet"
+    network_policy     = "calico"
+    dns_service_ip     = "172.16.0.10"
+    service_cidr       = "172.16.0.0/24"
+    docker_bridge_cidr = "172.16.1.1/32"
+  }
+
+  tags = {
+    builder = "pat"
+  }
+  lifecycle {
+    ignore_changes = [
+      key_vault_secrets_provider
+    ]
+  }
+
+  depends_on = [
+    azurerm_subnet_route_table_association.systempool,
+    azurerm_subnet_route_table_association.userpool,
+    azurerm_role_assignment.aks_contributor,
+    azurerm_role_assignment.aks_network_contributor
+  ]
+}
+
+resource "azurerm_user_assigned_identity" "aks" {
+  name                = "aks-identity"
+  location            = azurerm_resource_group.aks.location
+  resource_group_name = azurerm_resource_group.aks.name
+}
+
+resource "azurerm_role_assignment" "aks_contributor" {
+  scope                = azurerm_resource_group.aks.id
+  role_definition_name = "Contributor"
+  principal_id         = azurerm_user_assigned_identity.aks.principal_id
+}
+
+resource "azurerm_role_assignment" "aks_network_contributor" {
+  scope                = azurerm_resource_group.aks.id
+  role_definition_name = "Network Contributor"
+  principal_id         = azurerm_user_assigned_identity.aks.principal_id
+}
---
+++ aks/network.tf
@@ -0,0 +1,85 @@
+resource "azurerm_virtual_network" "spoke" {
+  name                = "spoke${random_id.nr.hex}"
+  location            = azurerm_resource_group.spoke.location
+  resource_group_name = azurerm_resource_group.spoke.name
+  address_space       = ["10.20.16.0/23"]
+}
+
+resource "azurerm_subnet" "systempool" {
+  name                 = "systempool-subnet"
+  resource_group_name  = azurerm_resource_group.spoke.name
+  virtual_network_name = azurerm_virtual_network.spoke.name
+  address_prefixes     = ["10.20.17.32/27"]
+}
+
+resource "azurerm_subnet_route_table_association" "systempool" {
+  subnet_id      = azurerm_subnet.systempool.id
+  route_table_id = azurerm_route_table.main.id
+}
+
+resource "azurerm_subnet" "userpool" {
+  name                 = "userpool-subnet"
+  resource_group_name  = azurerm_resource_group.spoke.name
+  virtual_network_name = azurerm_virtual_network.spoke.name
+  address_prefixes     = ["10.20.17.64/27"]
+}
+
+resource "azurerm_subnet_route_table_association" "userpool" {
+  subnet_id      = azurerm_subnet.userpool.id
+  route_table_id = azurerm_route_table.main.id
+}
+
+
+resource "azurerm_subnet" "data" {
+  name                 = "data-subnet"
+  resource_group_name  = azurerm_resource_group.spoke.name
+  virtual_network_name = azurerm_virtual_network.spoke.name
+  address_prefixes     = ["10.20.16.192/26"]
+}
+
+resource "azurerm_subnet_route_table_association" "data" {
+  subnet_id      = azurerm_subnet.data.id
+  route_table_id = azurerm_route_table.main.id
+}
+
+resource "azurerm_subnet" "spoke1end" {
+  name                 = "spoke1end-subnet"
+  resource_group_name  = azurerm_resource_group.spoke.name
+  virtual_network_name = azurerm_virtual_network.spoke.name
+  address_prefixes     = ["10.20.17.0/28"]
+}
+
+resource "azurerm_subnet_route_table_association" "spoke1end" {
+  subnet_id      = azurerm_subnet.spoke1end.id
+  route_table_id = azurerm_route_table.main.id
+}
+
+resource "azurerm_route_table" "main" {
+  name                = "main"
+  location            = azurerm_resource_group.aks.location
+  resource_group_name = azurerm_resource_group.aks.name
+
+  route {
+    name           = "azure"
+    address_prefix = "AzureCloud"
+    next_hop_type  = "Internet"
+  }
+
+  route {
+    name           = "mypip"
+    address_prefix = "84.104.239.28/32"
+    next_hop_type  = "Internet"
+  }
+  route {
+    name           = "mypip-fakkel"
+    address_prefix = "109.69.228.221/32"
+    next_hop_type  = "Internet"
+  }
+  lifecycle {
+    ignore_changes = [
+      # Ignore changes to tags, e.g. because a management agent
+      # updates these based on some ruleset managed elsewhere.
+      route
+    ]
+  }
+}
