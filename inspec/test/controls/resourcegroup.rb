b9a820ec4b0a0d757b91e754d894a4a1f3547509
Patrick Riedijk <patrick.riedijk@avanade.com>
2023-01-17 13:36:57 +0100

    correct secrets

--- inspec/test/controls/resourcegroup.rb
+++ inspec/test/controls/resourcegroup.rb
@@ -1,8 +1,8 @@
 # sample compliance test to verify the resource group exists or not
-control "azure-rg-exists-sample" do   
-    impact 0.1
-    desc "sample test to verify any resource group exists in the subscription"    
-    describe azure_resource_groups do
-      it { should exist }
-    end
-  end
+control 'azure_resource_group' do 
+  title "Check resource group" 
+  desc "Check if resource group is present" 
+  describe azure_resource_group(name: 'tfstatetestfakkel') do 
+  it { should exist } 
+  end 
+end 
