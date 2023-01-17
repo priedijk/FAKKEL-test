22b1f4cbc630c79eee20f73f330ce6a214ef2d8a
Patrick Riedijk <patrick.riedijk@avanade.com>
2023-01-17 16:55:14 +0100

    test

--- inspec/test/controls/keyvault.rb
+++ inspec/test/controls/keyvault.rb
@@ -1,3 +1,11 @@
 describe azure_key_vault(resource_group: 'fakkel-kv', name: +input('KEYVAULT')) do
+    it { should exist }  
+  end     
+
+describe azure_key_vault(resource_group: 'fakkel-kv', name: +input('KEYVAULT')) do
+    its('properties.enabledForDiskEncryption') { should be_true }  
+  end     
+
+describe azure_key_vault(resource_group: 'fakkel-kv', name: +input('KEYVAULT')) do
     its('properties.privateEndpointConnections.properties.privateEndpointConnections') { should eq 'Succeeded' }
   end            
