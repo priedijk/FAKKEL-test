afb25da8033bc5a221e556a1bd65cb2bf5738fe0
Patrick Riedijk <patrick.riedijk@avanade.com>
2023-01-18 13:34:20 +0100

    fix end

--- inspec/test/controls/hubtohubpeering.rb
+++ inspec/test/controls/hubtohubpeering.rb
@@ -1,18 +1,19 @@
-control 'hub_to_hub_peering' do 
-    title "Check peering status" 
-  
-  describe azure_virtual_network_peering(resource_group: 'rg-appserviceplanintegration', vnet: 'appsvc', name: 'testpeer') do
-      its('properties.peeringSyncLevel') { should eq 'FullyInSync' }  
-    end   
+control 'hub_to_hub_peering' do
+    title "Check peering status"
 
   describe azure_virtual_network_peering(resource_group: 'rg-appserviceplanintegration', vnet: 'appsvc', name: 'testpeer') do
-      its('properties.provisioningState') { should eq 'Succeeded' }  
-    end   
+      its('properties.peeringSyncLevel') { should eq 'FullyInSync' }
+    end
+
+  describe azure_virtual_network_peering(resource_group: 'rg-appserviceplanintegration', vnet: 'appsvc', name: 'testpeer') do
+      its('properties.provisioningState') { should eq 'Succeeded' }
+    end
 
   describe azure_virtual_network_peering(resource_group: 'Ansible', vnet: 'Ansible-VMVNET', name: 'testpeer2') do
-      its('properties.peeringSyncLevel') { should eq 'FullyInSync' }  
-    end   
+      its('properties.peeringSyncLevel') { should eq 'FullyInSync' }
+    end
 
     describe azure_virtual_network_peering(resource_group: 'Ansible', vnet: 'Ansible-VMVNET', name: 'testpeer2') do
-      its('properties.provisioningState') { should eq 'Succeeded' }  
-    end   
+      its('properties.provisioningState') { should eq 'Succeeded' }
+    end
+  end
