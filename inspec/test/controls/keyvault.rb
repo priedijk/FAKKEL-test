e4ae4f58d5a72d64a106a8a9dc114c5d841e42eb
Patrick Riedijk <patrick.riedijk@avanade.com>
2023-01-18 09:19:37 +0100

    test

--- inspec/test/controls/keyvault.rb
+++ inspec/test/controls/keyvault.rb
@@ -21,3 +21,19 @@ describe azure_key_vault(resource_group: 'fakkel-kv', name: +input('KEYVAULT')) 
 describe azure_key_vault(resource_group: 'fakkel-kv', name: +input('KEYVAULT')) do
     its('properties.privateEndpointConnections[0].privateLinkServiceConnectionState.status') { should eq 'Approved' }
   end            
+
+
+describe azure_key_vault(resource_group: 'fakkel-kv', name: +input('KEYVAULT')) do
+    its('properties.privateEndpointConnections[].provisioningState') { should eq 'Succeeded' }
+  end   
+
+describe azure_key_vault(resource_group: 'fakkel-kv', name: +input('KEYVAULT')) do
+    its('properties.privateEndpointConnections[].privateLinkServiceConnectionState.status') { should eq 'Approved' }
+  end            
+describe azure_key_vault(resource_group: 'fakkel-kv', name: +input('KEYVAULT')) do
+    its('properties.privateEndpointConnections[*].provisioningState') { should eq 'Succeeded' }
+  end   
+
+describe azure_key_vault(resource_group: 'fakkel-kv', name: +input('KEYVAULT')) do
+    its('properties.privateEndpointConnections[*].privateLinkServiceConnectionState.status') { should eq 'Approved' }
+  end            
