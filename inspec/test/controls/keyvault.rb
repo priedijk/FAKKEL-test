ec90730cbaafe686ea42d5a14d5d8c07528c7f34
Patrick Riedijk <patrick.riedijk@avanade.com>
2023-01-18 09:33:13 +0100

    test

--- inspec/test/controls/keyvault.rb
+++ inspec/test/controls/keyvault.rb
@@ -1,6 +1,13 @@
 # describe azure_key_vault(resource_group: 'fakkel-kv', name: +input('KEYVAULT')) do
 #     it { should exist }  
-#   end     
+#   end  
+
+keyvaultProperties = azure_key_vault(resource_group: 'fakkel-kv', name: +input('KEYVAULT'))
+privateEndpointConnections = keyvaultProperties["privateEndpointConnections"]
+privateEndpointConnectionsToo = keyvaultProperties["privateEndpointConnections[0]"]
+
+control 'azure_key_vault' do 
+  title "Check Azure Keyvault" 
 
 describe azure_key_vault(resource_group: 'fakkel-kv', name: +input('KEYVAULT')) do
     its('properties.enabledForDiskEncryption') { should be_truthy }  
@@ -20,20 +27,21 @@ describe azure_key_vault(resource_group: 'fakkel-kv', name: +input('KEYVAULT')) 
 
 describe azure_key_vault(resource_group: 'fakkel-kv', name: +input('KEYVAULT')) do
     its('properties.privateEndpointConnections[0].privateLinkServiceConnectionState.status') { should eq 'Approved' }
-  end            
+  end  
 
+describe privateEndpointConnections do
+  its('provisioningState') { should eq 'Succeeded' }   
+  end        
 
-describe azure_key_vault(resource_group: 'fakkel-kv', name: +input('KEYVAULT')) do
-    its('properties.privateEndpointConnections[].provisioningState') { should eq 'Succeeded' }
-  end   
+describe privateEndpointConnectionsToo do
+  its('provisioningState') { should eq 'Succeeded' }   
+  end        
 
-describe azure_key_vault(resource_group: 'fakkel-kv', name: +input('KEYVAULT')) do
-    its('properties.privateEndpointConnections[].privateLinkServiceConnectionState.status') { should eq 'Approved' }
-  end            
-describe azure_key_vault(resource_group: 'fakkel-kv', name: +input('KEYVAULT')) do
-    its('properties.privateEndpointConnections[*].provisioningState') { should eq 'Succeeded' }
-  end   
+describe privateEndpointConnections do
+  its('privateLinkServiceConnectionState.status') { should eq 'Approved' }   
+  end        
 
-describe azure_key_vault(resource_group: 'fakkel-kv', name: +input('KEYVAULT')) do
-    its('properties.privateEndpointConnections[*].privateLinkServiceConnectionState.status') { should eq 'Approved' }
-  end            
+describe privateEndpointConnectionsToo do
+  its('privateLinkServiceConnectionState.status') { should eq 'Approved' }   
+  end        
+end
