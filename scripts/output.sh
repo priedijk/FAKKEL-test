8946bf7ec14314acd601e7d61771ba06913710d5
Patrick Riedijk <patrick.riedijk@avanade.com>
2023-01-19 15:19:05 +0100

    test

--- scripts/output.sh
+++ scripts/output.sh
@@ -1,14 +1,9 @@
 # export the values as environment variables
 export TERRAFORM_DIR=$TERRAFORM_DIRECTORY
-export KEYVAULT_NAME=$KEYVAULT
-export KEYVAULT_NAME2=$KEYVAULT2
-export KEYVAULT_NAME3=$KEYVAULT3
 
-echo "Keyvault name is $KEYVAULT"
-echo "Keyvault name is $KEYVAULT2"
-echo "Keyvault name is $KEYVAULT3"
 echo "terra init env $TERRAFORM_DIRECTORY"
 echo "after init env"
+echo "terra after env $TERRAFORM_DIR"
 
 terraform -chdir=$TERRAFORM_DIR output -raw kv_name
 echo "after command 1"
@@ -23,7 +18,7 @@ export KEYVAULT=$(terraform -chdir=$TERRAFORM_DIR output -raw kv_name)
 echo "KEYVAULT=$(echo $KEYVAULT)" >> $GITHUB_ENV
 echo "KEYVAULT2=$(terraform -chdir=$TERRAFORM_DIR output -raw kv_name)" >> $GITHUB_ENV
 echo "KEYVAULT3=${KEYVAULT}" >> $GITHUB_ENV
-echo "Keyvault name is $KEYVAULT_NAME"
-echo "Keyvault name is $KEYVAULT_NAME2"
-echo "Keyvault name is $KEYVAULT_NAME3"
+echo "Keyvault name is $KEYVAULT"
+echo "Keyvault name is $KEYVAULT2"
+echo "Keyvault name is $KEYVAULT3"
 exit 0
