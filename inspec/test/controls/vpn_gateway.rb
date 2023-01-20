bd36856bbfe09a6d08384d65ce34a6042ae039cc
Patrick Riedijk <patrick.riedijk@avanade.com>
2023-01-20 14:13:52 +0100

    comment unused tests

--- inspec/test/controls/vpn_gateway.rb
+++ inspec/test/controls/vpn_gateway.rb
@@ -1,7 +1,7 @@
-control 'hub_to_hub_peering' do
-    title "Check peering status"
+# control 'hub_to_hub_peering' do
+#     title "Check peering status"
 
-  describe azure_virtual_network_gateway_connection(resource_group: 'rg-appserviceplanintegration', name: 'testpeer') do
-      its('properties.connectionStatus') { should eq 'Connected' }
-    end
-  end
+#   describe azure_virtual_network_gateway_connection(resource_group: 'rg-appserviceplanintegration', name: 'testpeer') do
+#       its('properties.connectionStatus') { should eq 'Connected' }
+#     end
+#   end
