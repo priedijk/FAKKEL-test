1150128246a87d34b671825411b702a5e2b49d56
Patrick Riedijk <patrick.riedijk@avanade.com>
2023-01-20 13:52:15 +0100

    remove wrappers

---
+++ inspec/test/controls/onPremConnection.rb
@@ -0,0 +1,14 @@
+Verify host name is reachable over a specific protocol and port number 
+
+describe host('example.com', port: 80, protocol: 'tcp') do
+  it { should be_reachable }
+end
+
+
+Review the connection setup and socket contents when checking reachability 
+
+describe host('example.com', port: 12345, protocol: 'tcp') do
+  it { should be_reachable }
+  its('connection') { should_not match /connection refused/ }
+  its('socket') { should match /STATUS_OK/ }
+end
