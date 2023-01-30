f86801fe0bff2d52980bc796cae3f82786570541
Patrick Riedijk <patrick.riedijk@avanade.com>
2023-01-30 09:58:37 +0100

    test with input file

--- inspec/test/controls/keyvault.rb
+++ inspec/test/controls/keyvault.rb
@@ -9,6 +9,13 @@ describe azure_key_vault(resource_group: 'fakkel-kv', name: +input('KEYVAULT')) 
     its('properties.enabledForDiskEncryption') { should be_truthy }  
   end   
 
+describe azure_key_vault(resource_group: 'fakkel-kv', name: +input('keyvault')) do
+    its('properties.enabledForDiskEncryption') { should be_truthy }  
+  end   
+describe azure_key_vault(resource_group: 'fakkel-kv', name: +input('keyvault_name')) do
+    its('properties.enabledForDiskEncryption') { should be_truthy }  
+  end   
+
 
 
   privateEndpointConnections = azure_key_vault(resource_group: 'fakkel-kv', name: +input('KEYVAULT')).properties.privateEndpointConnections
--- scripts/chef.sh
+++ scripts/chef.sh
@@ -12,10 +12,15 @@ echo "Keyvault name is $KEYVAULT"
 # install the Chef Inspec
 curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -P inspec 
 
-
-
+declare -A assArray1
+assArray1[fruit]=Mango
+assArray1[bird]=Cockatail
+assArray1[flower]=Rose
+assArray1[animal]=Tiger
 # execute Azure tests
-inspec exec inspec/test/ --input=URL="kv-test-weu-50e2b310.vault.azure.net" KEYVAULT=$KEYVAULT -t azure:// --chef-license accept-silent --reporter cli html:azure_test.html 
+# inspec exec inspec/test/ --input=URL="kv-test-weu-50e2b310.vault.azure.net" KEYVAULT=$KEYVAULT -t azure:// --chef-license accept-silent --reporter cli html:azure_test.html 
+
+inspec exec inspec/test/ --input scripts/inputs.yaml -t azure:// --chef-license accept-silent --reporter cli html:azure_test.html 
 
 # execute keyvault test
 # inspec exec inspec/access/ --input=URL="kv-test-weu-50e2b310.vault.azure.net" KEYVAULT=$KEYVAULT --chef-license accept-silent --reporter cli html:keyvault_test.html 
