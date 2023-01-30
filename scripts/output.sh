d9ee9eef9338d0275af6110d8c0adc36e51b3af2
Patrick Riedijk <patrick.riedijk@avanade.com>
2023-01-30 10:03:32 +0100

    less

--- scripts/output.sh
+++ scripts/output.sh
@@ -1,32 +1,32 @@
 # export the values as environment variables
 export TERRAFORM_DIR=$TERRAFORM_DIRECTORY
 
-echo "terra init env $TERRAFORM_DIRECTORY"
-echo "after init env"
-echo "terra after env $TERRAFORM_DIR"
+# echo "terra init env $TERRAFORM_DIRECTORY"
+# echo "after init env"
+# echo "terra after env $TERRAFORM_DIR"
 
-terraform -chdir=$TERRAFORM_DIR output -raw kv_name
-echo "only direct env 1"
-export KEYVAULT=$(terraform -chdir=$TERRAFORM_DIRECTORY output -raw kv_name)
-echo $KEYVAULT
-echo "only direct env 2"
-export KEYVAULT=$(terraform -chdir=${TERRAFORM_DIRECTORY} output -raw kv_name)
-echo $KEYVAULT
-echo "only direct env 3"
-export KEYVAULT=$(terraform -chdir=$TERRAFORM_DIRECTORY output -raw kv_name)
-echo $KEYVAULT
-
-
+# terraform -chdir=$TERRAFORM_DIR output -raw kv_name
+# echo "only direct env 1"
+# export KEYVAULT=$(terraform -chdir=$TERRAFORM_DIRECTORY output -raw kv_name)
+# echo $KEYVAULT
+# echo "only direct env 2"
+# export KEYVAULT=$(terraform -chdir=${TERRAFORM_DIRECTORY} output -raw kv_name)
+# echo $KEYVAULT
+# echo "only direct env 3"
+# export KEYVAULT=$(terraform -chdir=$TERRAFORM_DIRECTORY output -raw kv_name)
+# echo $KEYVAULT
 
 
-echo "after command 1"
-export KEYVAULT=$(terraform -chdir=$TERRAFORM_DIR output -raw kv_name)
-echo $KEYVAULT
-echo "after command 2"
-export KEYVAULT=$(terraform -chdir=${TERRAFORM_DIR} output -raw kv_name)
-echo $KEYVAULT
-echo "after command 3"
-export KEYVAULT=$(terraform -chdir=$TERRAFORM_DIR output -raw kv_name)
+
+
+# echo "after command 1"
+# export KEYVAULT=$(terraform -chdir=$TERRAFORM_DIR output -raw kv_name)
+# echo $KEYVAULT
+# echo "after command 2"
+# export KEYVAULT=$(terraform -chdir=${TERRAFORM_DIR} output -raw kv_name)
+# echo $KEYVAULT
+# echo "after command 3"
+# export KEYVAULT=$(terraform -chdir=$TERRAFORM_DIR output -raw kv_name)
 
 echo "KEYVAULT=$(echo $KEYVAULT)" >> $GITHUB_ENV
 echo "KEYVAULT2=$(terraform -chdir=$TERRAFORM_DIR output -raw kv_name)" >> $GITHUB_ENV
