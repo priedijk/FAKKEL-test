4f25689a87b9c0043a09c6f2c2e773a904afc3f1
Patrick Riedijk <patrick.riedijk@avanade.com>
2023-01-30 10:26:56 +0100

    test

--- scripts/chef.sh
+++ scripts/chef.sh
@@ -4,6 +4,7 @@
 # export AZURE_TENANT_ID=$INPUT_TENANTID
 # export AZURE_CLIENT_SECRET=$INPUT_CLIENTSECRET
 export KEYVAULT_NAME=$KEYVAULT
+export KEYVAULT=$KEYVAULT
 
 echo "Keyvault name is $KEYVAULT_NAME"
 echo "Keyvault name is $KEYVAULT"
@@ -23,8 +24,9 @@ curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -P inspec
 
 echo scripts/inputs.yaml
 envsubst < scripts/inputs.yaml
+envsubst < scripts/inputs.yaml > scripts/inputs-sub.yaml
 
-inspec exec inspec/test/ --input-file scripts/inputs.yaml -t azure:// --chef-license accept-silent --reporter cli html:azure_test.html 
+inspec exec inspec/test/ --input-file scripts/inputs-sub.yaml -t azure:// --chef-license accept-silent --reporter cli html:azure_test.html 
 
 # execute keyvault test
 # inspec exec inspec/access/ --input=URL="kv-test-weu-50e2b310.vault.azure.net" KEYVAULT=$KEYVAULT --chef-license accept-silent --reporter cli html:keyvault_test.html 
