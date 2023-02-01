f394a9305cfd104f04836fbd241267822da5ff7c
Patrick Riedijk <patrick.riedijk@avanade.com>
2023-02-01 09:30:39 +0100

    test with specific inspec version

--- scripts/chef.sh
+++ scripts/chef.sh
@@ -10,14 +10,13 @@ echo "Keyvault name is $KEYVAULT_NAME"
 echo "Keyvault name is $KEYVAULT"
 # exit 0
 
-# install the Chef Inspec
-curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -P inspec 
+# # install the Chef Inspec
+# curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -P inspec 
 
-# declare -A assArray1
-# assArray1[fruit]=Mango
-# assArray1[bird]=Cockatail
-# assArray1[flower]=Rose
-# assArray1[animal]=Tiger
+# test install specific version
+wget -q https://packages.chef.io/files/stable/inspec/5.21.29/ubuntu/20.04/inspec_5.21.29-1_amd64.deb
+echo 35ed1da6c885edd1618c19eb0305eaf2836cfae33b21ebd327d31512f15140d7 inspec_5.21.29-1_amd64.deb|sha256sum -c  
+sudo dpkg -i inspec_5.21.29-1_amd64.deb
 
 # execute Azure tests
 # inspec exec inspec/test/ --input=URL="kv-test-weu-50e2b310.vault.azure.net" KEYVAULT=$KEYVAULT -t azure:// --chef-license accept-silent --reporter cli html:azure_test.html 
@@ -29,7 +28,7 @@ envsubst < inspec/test/inputs.yaml
 TEST_VAR="test"
 TEST_DIRECTORY="inspec/${TEST_VAR}"
 
-inspec exec ${TEST_DIRECTORY} --input-file ${TEST_DIRECTORY}/inputs-sub.yaml -t azure:// --chef-license accept-silent --reporter progress-bar html:azure_test.html --enhanced-outcomes
+inspec exec ${TEST_DIRECTORY} --input-file ${TEST_DIRECTORY}/inputs-sub.yaml -t azure:// --chef-license accept-silent --reporter cli html:azure_test.html
 
 # execute keyvault test
 # inspec exec inspec/access/ --input=URL="kv-test-weu-50e2b310.vault.azure.net" KEYVAULT=$KEYVAULT --chef-license accept-silent --reporter cli html:keyvault_test.html 
