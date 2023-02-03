fc7de0730060925713a8c9a60d87827d72227108
Patrick Riedijk <patrick.riedijk@avanade.com>
2023-02-03 13:39:05 +0100

    kv remove test

--- kv-test/output.tf
+++ kv-test/output.tf
@@ -2,10 +2,10 @@ output "kvhex" {
   value = random_id.kvname.hex
 }
 
-output "rg_name" {
-  value = azurerm_resource_group.fakkel.name
-}
+# output "rg_name" {
+#   value = azurerm_resource_group.fakkel.name
+# }
 
-output "kv_name" {
-  value = azurerm_key_vault.fakkel.name
-}
+# output "kv_name" {
+#   value = azurerm_key_vault.fakkel.name
+# }
