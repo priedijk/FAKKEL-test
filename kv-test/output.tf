ac585d27f9b7cb8c33b648a93c7493b62c7c0aaa
Patrick Riedijk <patrick.riedijk@avanade.com>
2023-02-03 13:56:48 +0100

    read outputs

--- kv-test/output.tf
+++ kv-test/output.tf
@@ -1,11 +1,11 @@
-# output "kvhex" {
-#   value = random_id.kvname.hex
-# }
+output "kvhex" {
+  value = random_id.kvname.hex
+}
 
-# output "rg_name" {
-#   value = azurerm_resource_group.fakkel.name
-# }
+output "rg_name" {
+  value = azurerm_resource_group.fakkel.name
+}
 
-# output "kv_name" {
-#   value = azurerm_key_vault.fakkel.name
-# }
+output "kv_name" {
+  value = azurerm_key_vault.fakkel.name
+}
