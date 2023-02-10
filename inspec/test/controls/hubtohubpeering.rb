36c59d4f2ba9f2676d8434cd9100ce103b043ced
Patrick Riedijk <patrick.riedijk@avanade.com>
2023-02-10 14:20:05 +0100

    version test

--- inspec/test/controls/hubtohubpeering.rb
+++ inspec/test/controls/hubtohubpeering.rb
@@ -1,19 +1,19 @@
-control 'hub_to_hub_peering' do
-    title "Check peering status"
+# control 'hub_to_hub_peering' do
+#     title "Check peering status"
 
-  describe azure_virtual_network_peering(resource_group: 'rg-appserviceplanintegration', vnet: 'appsvc', name: 'testpeer') do
-      its('properties.peeringSyncLevel') { should eq 'FullyInSync' }
-    end
+#   describe azure_virtual_network_peering(resource_group: 'rg-appserviceplanintegration', vnet: 'appsvc', name: 'testpeer') do
+#       its('properties.peeringSyncLevel') { should eq 'FullyInSync' }
+#     end
 
-  describe azure_virtual_network_peering(resource_group: 'rg-appserviceplanintegration', vnet: 'appsvc', name: 'testpeer') do
-      its('properties.provisioningState') { should eq 'Succeeded' }
-    end
+#   describe azure_virtual_network_peering(resource_group: 'rg-appserviceplanintegration', vnet: 'appsvc', name: 'testpeer') do
+#       its('properties.provisioningState') { should eq 'Succeeded' }
+#     end
 
-  describe azure_virtual_network_peering(resource_group: 'Ansible', vnet: 'Ansible-VMVNET', name: 'testpeer2') do
-      its('properties.peeringSyncLevel') { should eq 'FullyInSync' }
-    end
+#   describe azure_virtual_network_peering(resource_group: 'Ansible', vnet: 'Ansible-VMVNET', name: 'testpeer2') do
+#       its('properties.peeringSyncLevel') { should eq 'FullyInSync' }
+#     end
 
-    describe azure_virtual_network_peering(resource_group: 'Ansible', vnet: 'Ansible-VMVNET', name: 'testpeer2') do
-      its('properties.provisioningState') { should eq 'Succeeded' }
-    end
-  end
+#     describe azure_virtual_network_peering(resource_group: 'Ansible', vnet: 'Ansible-VMVNET', name: 'testpeer2') do
+#       its('properties.provisioningState') { should eq 'Succeeded' }
+#     end
+#   end
--- inspec/test/controls/resourcegroup.rb
+++ inspec/test/controls/resourcegroup.rb
@@ -1,8 +1,8 @@
-# sample compliance test to verify the resource group exists or not
-control 'azure_resource_group' do 
-  title "Check resource group" 
-  desc "Check if resource group is present" 
-  describe azure_resource_group(name: 'tfstatetestfakkel') do 
-  it { should exist } 
-  end 
-end 
+# # sample compliance test to verify the resource group exists or not
+# control 'azure_resource_group' do 
+#   title "Check resource group" 
+#   desc "Check if resource group is present" 
+#   describe azure_resource_group(name: 'tfstatetestfakkel') do 
+#   it { should exist } 
+#   end 
+# end 
