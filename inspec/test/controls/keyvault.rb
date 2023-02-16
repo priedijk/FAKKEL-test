906bda7ea0d2e2aac75665a322e94586fcb0c656
Patrick Riedijk <patrick.riedijk@avanade.com>
2023-02-16 11:38:13 +0100

    test

--- inspec/test/controls/keyvault.rb
+++ inspec/test/controls/keyvault.rb
@@ -1,27 +1,52 @@
 control 'azure_key_vault' do
   title "Check Azure Keyvault"
 
-# describe azure_key_vault(resource_group: +input('RG'), name: +input('KEYVAULT')) do
-#     its('properties.enabledForDiskEncryption') { should be_truthy }
-#   end
+describe azure_key_vault(resource_group: +input('RG'), name: +input('KEYVAULT')) do
+    its('properties.enabledForDiskEncryption') { should be_truthy }
+  end
 
-#   privateEndpointConnections = azure_key_vault(resource_group: 'fakkel-kv', name: +input('KEYVAULT')).properties.privateEndpointConnections
+  privateEndpointConnections = azure_key_vault(resource_group: 'fakkel-kv', name: +input('KEYVAULT')).properties.privateEndpointConnections
 
-#   privateEndpointConnections.each do |endpoints|
-#     describe endpoints do
-#       its('properties.provisioningState') { should eq 'Succeeded' }
-#     end
-#   end
+  privateEndpointConnections.each do |endpoints|
+    describe endpoints do
+      its('properties.provisioningState') { should eq 'Succeeded' }
+    end
+  end
 
-#   privateEndpointConnections.each do |privateLink|
-#     describe privateLink do
-#       its('properties.privateLinkServiceConnectionState.status') { should eq 'Approved' }
-#     end
-#   end
+  privateEndpointConnections.each do |privateLink|
+    describe privateLink do
+      its('properties.privateLinkServiceConnectionState.status') { should eq 'Approved' }
+    end
+  end
+
+
+
+
+
+
+
+
+
+  # testing
+  keyvaults = azure_generic_resources(resource_provider: 'Microsoft.KeyVault/vaults').ids
+
+  keyvaults.each do |id|
+
+    describe azure_key_vault(resource_id: id) do
+      its('properties.enabledForDiskEncryption') { should be_truthy }
+    end
+  end
+
+
+
+
+
+
 
   azure_generic_resources(resource_provider: 'Microsoft.KeyVault/vaults').ids.each do |id|
     describe azure_generic_resource(resource_id: id) do
 
+      
         # disk encryption
         describe azure_key_vault(resource_group: +input('RG'), name: +input('KEYVAULT')) do
           its('properties.enabledForDiskEncryption') { should be_truthy }
