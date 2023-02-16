b206fbde2af218ef0917d1ab1d28e86543cdfcee
Patrick Riedijk <patrick.riedijk@avanade.com>
2023-02-16 11:26:22 +0100

    test generic all keyvaults

--- inspec/test/controls/keyvault.rb
+++ inspec/test/controls/keyvault.rb
@@ -1,36 +1,46 @@
-# describe azure_key_vault(resource_group: 'fakkel-kv', name: +input('KEYVAULT')) do
-#     it { should exist }  
-#   end  
+control 'azure_key_vault' do
+  title "Check Azure Keyvault"
 
-control 'azure_key_vault' do 
-  title "Check Azure Keyvault" 
+# describe azure_key_vault(resource_group: +input('RG'), name: +input('KEYVAULT')) do
+#     its('properties.enabledForDiskEncryption') { should be_truthy }
+#   end
 
-describe azure_key_vault(resource_group: +input('RG'), name: +input('KEYVAULT')) do
-    its('properties.enabledForDiskEncryption') { should be_truthy }  
-  end   
+#   privateEndpointConnections = azure_key_vault(resource_group: 'fakkel-kv', name: +input('KEYVAULT')).properties.privateEndpointConnections
 
+#   privateEndpointConnections.each do |endpoints|
+#     describe endpoints do
+#       its('properties.provisioningState') { should eq 'Succeeded' }
+#     end
+#   end
 
+#   privateEndpointConnections.each do |privateLink|
+#     describe privateLink do
+#       its('properties.privateLinkServiceConnectionState.status') { should eq 'Approved' }
+#     end
+#   end
 
-  privateEndpointConnections = azure_key_vault(resource_group: 'fakkel-kv', name: +input('KEYVAULT')).properties.privateEndpointConnections
-  
-  privateEndpointConnections.each do |endpoints|
-    describe endpoints do
-      its('properties.provisioningState') { should eq 'Succeeded' }
-    end 
-  end
+  azure_generic_resources(resource_provider: 'Microsoft.KeyVault/vaults').ids.each do |id|
+    describe azure_generic_resource(resource_id: id) do
 
-  privateEndpointConnections.each do |privateLink|
-    describe privateLink do
-      its('properties.privateLinkServiceConnectionState.status') { should eq 'Approved' }
+        # disk encryption
+        describe azure_key_vault(resource_group: +input('RG'), name: +input('KEYVAULT')) do
+          its('properties.enabledForDiskEncryption') { should be_truthy }
+        end
+
+        # private endpoints
+        privateEndpointConnections = azure_key_vault(resource_group: 'fakkel-kv', name: +input('KEYVAULT')).properties.privateEndpointConnections
+
+        privateEndpointConnections.each do |endpoints|
+          describe endpoints do
+            its('properties.provisioningState') { should eq 'Succeeded' }
+          end
+        end
+
+        privateEndpointConnections.each do |privateLink|
+          describe privateLink do
+            its('properties.privateLinkServiceConnectionState.status') { should eq 'Approved' }
+          end
+        end
     end
   end
-
-# describe azure_key_vault(resource_group: 'fakkel-kv', name: +input('KEYVAULT')) do
-#     its('properties.privateEndpointConnections.provisioningState') { should eq 'Succeeded' }
-#   end   
-
-# describe azure_key_vault(resource_group: 'fakkel-kv', name: +input('KEYVAULT')) do
-#     its('properties.privateEndpointConnections.privateLinkServiceConnectionState.status') { should eq 'Approved' }
-#   end  
-
 end
