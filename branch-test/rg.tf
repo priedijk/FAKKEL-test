fa805559e8275cd4cfb869c82982ff8e4d06f6b3
Patrick Riedijk <patrick.riedijk@avanade.com>
2023-02-22 18:22:17 +0100

    more cleanup

--- branch-test/rg.tf
+++
@@ -1,4 +0,0 @@
-resource "azurerm_resource_group" "branch-group-rg" {
-  name     = "branch-test"
-  location = var.resource_group_location
-}
--- output/rg.tf
+++
@@ -1,4 +0,0 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-output"
-  location = var.resource_group_location
-}
