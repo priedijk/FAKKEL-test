f6c40a4e79dd360e1cebba916b8ffc58ac248356
Patrick Riedijk <patrick.riedijk@avanade.com>
2023-11-10 10:59:17 +0100

    validation test

--- scripts/input_output.sh
+++ scripts/input_output.sh
@@ -17,3 +17,35 @@ echo "Zip password  = ${zipPassword}"
 echo "Token access = ${tokenAccess}"
 echo "Token validity = ${tokenValidity}"
 echo "-----------------------------------------------------------------------------------"
+
+# validation
+if [[ -z ${fileShareName}  && -z ${containerName} ]]; then
+    echo "------------------------------------------------------------------------------------------------------"
+    echo "A Fileshare or Blob container name must be given as an input"
+    echo "------------------------------------------------------------------------------------------------------"
+
+    {
+    echo "------------------------------------------------------------------------------------------------------"
+    echo "#### A Fileshare or Blob container name must be given as an input"
+    echo "------------------------------------------------------------------------------------------------------"
+    } >> $GITHUB_STEP_SUMMARY 
+fi
+
+if [[ "${fileShareName}" != ""  &&  "${containerName}" != "" ]]; then
+    echo "------------------------------------------------------------------------------------------------------"
+    echo "Only one of Fileshare or Blob container name can be given as an input"
+    echo "------------------------------------------------------------------------------------------------------"
+
+    {
+    echo "------------------------------------------------------------------------------------------------------"
+    echo "#### Only one of Fileshare or Blob container name can be given as an input"
+    echo "------------------------------------------------------------------------------------------------------"
+    } >> $GITHUB_STEP_SUMMARY
+fi
+
+
+# $tfStateStorage = az storage account list --query "[?starts_with(name,'sttfstate') && location=='westeurope'].name" --subscription ${{ env.ARM_SUBSCRIPTION_ID }} -o tsv
+# if (-not $tfStateStorage) {
+#     Write-Error "ERROR Could not determine storage account for Terraform state."
+#     Exit 1
+# }
