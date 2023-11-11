758bda9a0e923327c957d52cb2d82330497aff98
priedijk <patrickriedijk@gmail.com>
2023-11-11 23:10:12 +0100

    revert

--- scripts/input_output.ps1
+++ scripts/input_output.ps1
@@ -1,7 +1,7 @@
 [CmdletBinding()]
 param(
     [String] [Parameter (Mandatory = $true)]  $storageAccountName,
-    [SecureString] [Parameter (Mandatory = $true)]  $zipPassword,
+    [String] [Parameter (Mandatory = $true)]  $zipPassword,
     [String] [Parameter (Mandatory = $true)]  $tokenAccess,
     [String] [Parameter (Mandatory = $true)]  $tokenValidity,
     [String] $fileShareName,
