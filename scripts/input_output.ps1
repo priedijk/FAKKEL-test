f1c336ee999aa8c5cbc41e598ec84fc47ef25d45
priedijk <patrickriedijk@gmail.com>
2023-11-12 23:25:38 +0100

    diff output

--- scripts/input_output.ps1
+++ scripts/input_output.ps1
@@ -303,7 +303,7 @@ elseif ( ${tokenType} -eq "container" ) {
 ##########################################################################################################################################
 
 # Write variables to Github env
-Write-Output "SAS_TOKEN_TYPE=${tokenType}" >> $GITHUB_ENV
-Write-Output "SAS_TOKEN_ACCESS=${tokenAccess}" >> $GITHUB_ENV
-Write-Output "SAS_END_DATE=${endDateFormatted}" >> $GITHUB_ENV
-Write-Output "STORAGE_SAS_TOKEN=${sasToken}" >> $GITHUB_ENV
+"SAS_TOKEN_TYPE=${tokenType}" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf-8 -Append
+"SAS_TOKEN_ACCESS=${tokenAccess}" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf-8 -Append
+"SAS_END_DATE=${endDateFormatted}" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf-8 -Append
+"STORAGE_SAS_TOKEN=${sasToken}" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf-8 -Append
